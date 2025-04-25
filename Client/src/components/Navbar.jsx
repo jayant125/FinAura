b703ddd5f7d4c59d4110bb8aa0fa7b3da96ccd7a
jayant <jayant@kali>
2025-04-25 22:53:09 +0530

    setup 1.0

---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <div className="bg-amber-300">
+            <h1 className="bg-amber-300">Hello sir </h1>
+        </div>
+    )
+}
